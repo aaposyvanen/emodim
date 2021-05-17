6203bfbeebf895460e6c5ea69e64ddf1a5f951fa
Aapo <aapo.syvanen@gmail.com>
2021-05-17 14:55:17 +0300

    init dev branch

--- Aapo/emodim.py
+++ Aapo/emodim.py
@@ -1,5 +1,4 @@
 import libvoikko
-from libvoikko import Voikko
 import xml.etree.cElementTree as ET
 import os
 from wvlib_light import lwvlib
@@ -16,8 +15,8 @@ Consequently, bad things may happen if this is run in a production environment w
 """
 
 path = f"{os.getcwd()}\\Voikko"
-Voikko.setLibrarySearchPath(path)
-v = Voikko(u"fi", path)
+libvoikko.Voikko.setLibrarySearchPath(path)
+v = libvoikko.Voikko(u"fi", path)
 tree = ET.parse("soderholm_normalized.xml")
 # tree = ET.parse("soderholm_et_al.xml")
 root = tree.getroot()
@@ -36,9 +35,9 @@ def rate(word):
     for pattern in root.iter('pattern'):
         if pattern.attrib['word'] == word:
             # print(pattern.tag, pattern.attrib)
-            va = pattern.attrib['valence']
-            a = pattern.attrib['arousal']
-            d = pattern.attrib['dominance']
+            va = round(float(pattern.attrib['valence']), 3)
+            a = round(float(pattern.attrib['arousal']), 3)
+            d = round(float(pattern.attrib['dominance']), 3)
     return va, a, d
 
 
@@ -55,16 +54,18 @@ def findRatedSynonym(word, library):
                 baseform = find_baseform(n[1], v)
                 ratingResult = rate(baseform)
                 if ratingResult[0] is not None:
-                    return {"original_text": w, "nearest": n[1], "similarity": round(float(n[0]), 3), "baseform": baseform, "rating": ratingResult}
+                    return {"original_text": w, "nearest": n[1],
+                            "similarity": round(float(n[0]), 3), "baseform": baseform, "rating": ratingResult}
     return {"original_text": w, "nearest": None, "similarity": None, "baseform": None, "rating": ratingResult}
 
 
 def word_eval(word):
+    resultMap = {}
     t = v.tokens(word)
     baseform = find_baseform(t[0].tokenText, v)
-    resultMap = {}
     if t[0].tokenType != libvoikko.Token.WORD:
         resultMap = {"original_text": t[0].tokenText, "baseform": baseform, "rating": rate(t[0].tokenText)}
+        # print(f"Token was not of type WORD: {resultMap}")
         return resultMap
     if baseform is None:
         result = findRatedSynonym(t[0].tokenText, wv)
@@ -72,20 +73,20 @@ def word_eval(word):
         return result
     # fetch the ratings for valence, arousal and dominance for the baseform word
     rr = rate(baseform)
-    ratedSynonym = findRatedSynonym(t[0].tokenText, wv)
-    resultstring = f"Voikko: baseform: {baseform} v: {rr[0]} a: {rr[1]} d: {rr[2]}"
-    resultstring = f"{resultstring}\nParsebank: {ratedSynonym}"
-    # print(resultstring)
     if rr[0] is not None and rr[1] is not None and rr[2] is not None:
         # the presence of the 'direct_' fields shows that the parsebank would not have been needed.
         resultMap = {"original_text": t[0].tokenText, "baseform": baseform, "direct_valence": rr[0],
                      "direct_arousal": rr[1], "direct_dominance": rr[2], "rating": rr}
         # print(f"Rating came through for baseform: {resultMap}")
-
     else:
+        ratedSynonym = findRatedSynonym(t[0].tokenText, wv)
+        resultString = f"Voikko: baseform: {baseform} v: {rr[0]} a: {rr[1]} d: {rr[2]} Parsebank: {ratedSynonym}"
+        # print(resultString)
         if ratedSynonym is None:
             print(f"None found. (Word: {t[0].tokenText})")
+        # word_eval at this moment ends in this else statement most often (> 90% of the cases)
         else:
+            # finding ratedSynonym makes the iteration 70 times slower
             rs = ratedSynonym
             resultMap = {"original_text": t[0].tokenText, "baseform": baseform, "parsebank_nearest": rs['nearest'],
                          "nearest_baseform": rs['baseform'], "parsebank_nearest_distance": rs['similarity'],
@@ -93,5 +94,4 @@ def word_eval(word):
                          "parsebank_nearest_dominance": rs['rating'][2],
                          "rating": (rs['rating'][0], rs['rating'][1], rs['rating'][2])}
             # print(f"Rating did not come through for baseform and ratedSynonym was not None: {resultMap}")
-            # word_eval at this moment ends in this else statement most often (> 90% of the cases)
     return resultMap
--- Aapo/s24_classify.py
+++ Aapo/s24_classify.py
@@ -3,14 +3,25 @@ import xml.etree.cElementTree as ET
 from tqdm import tqdm
 import emodim as em
 import os
+from datetime import datetime
+
 
 """
 The Suomi24 data can be classified 
 """
 
-# path = "D:\\Work\\Data\\s24_2001.vrt" # this file is 3,5Gb
-# path = "D:\\Work\\Data\\s24_2017.vrt" # this file is 17Gb
-path = "test.vrt"
+path = "D:\\Work\\Data\\s24_2001.vrt"  # this file is 3,5Gb
+# path = "D:\\Work\\Data\\s24_2017.vrt"  # this file is 17Gb
+# path = "test.vrt"
+
+
+def createAnalyzationFiles():
+    time = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    fname = f"{path.replace('.vrt', f'_classified_{time}.txt')}"
+    tname = f"{path.replace('.vrt', f'_threadData_{time}.json')}"
+    open(fname, 'w').close()
+    open(tname, 'w').close()
+    return fname, tname
 
 
 def evaluate_s24_data(datadict, vsum, asum, dsum):
@@ -20,12 +31,13 @@ def evaluate_s24_data(datadict, vsum, asum, dsum):
         for word in item:
             evaluate = em.word_eval(word)
             paragraphValues.append(evaluate)
-            # with open(f"{path.replace('.vrt', '_classified.txt')}", 'a', encoding='utf8') as f:
-            #   f.write(f"{evaluate['original_text']}: {evaluate['rating']} \n")
+            with open(ftxt, 'a+', encoding='utf8') as f:
+                f.write(f"{evaluate['original_text']}: {evaluate['rating']} \n")
             try:
                 vsum += float(evaluate['rating'][0])
                 asum += float(evaluate['rating'][1])
                 dsum += float(evaluate['rating'][2])
+            # if the word that was processed had None values in the rating list
             except TypeError:
                 continue
     return paragraphValues, round(vsum, 3), round(asum, 3), round(dsum, 3)
@@ -34,7 +46,7 @@ def evaluate_s24_data(datadict, vsum, asum, dsum):
 def s24_parser(dpath):
     # 2,748,069 iterations for s24 2001 dataset, around 4-10 it/s to process (around 130 hours), has to be optimized
     # the evaluate_s24_data function slows iteration down by over 75k it/s. word_eval function in emodim.py needs work.
-    # Could be because the data is located in HDD not SSD
+    # Could be because the data is located in HDD not SSD. Saved JSON for the 2001 set will be around 160Gb
     threadData = {}
     textData = {}
     paragraphData = {}
@@ -47,9 +59,10 @@ def s24_parser(dpath):
     for event, element in tqdm(context):
         if event == 'end' and element.tag == 'root':
             break
-        # a new textblock starts
+        # a new text block starts
         if event == 'start' and element.tag == 'text':
             if element.attrib['comment_id'] == "0":
+                threadData.clear()
                 threadData[element.attrib['thread_id']] = []
             vsum = asum = dsum = 0
             # save all of the metadata in textData
@@ -71,19 +84,15 @@ def s24_parser(dpath):
                     wordlist.clear()
                 ev, el = next(context)
             r.clear()
-        # do stuff to paragraphData here before it is emptied (and textData which contains metadata of the commentchain)
+        # do stuff to paragraphData here before it's emptied (and textData which contains metadata of the comment chain)
         elif event == 'end' and element.tag == 'text':
             pData, pvsum, pasum, pdsum = evaluate_s24_data(paragraphData, vsum, asum, dsum)
             threadData[textData['thread_id']].append(pData)
+            wordlist.clear(), textData.clear(), paragraphData.clear(), r.clear()
             # print(pData, pvsum, pasum, pdsum)
-            wordlist.clear()
-            textData.clear()
-            paragraphData.clear()
-            r.clear()
-    """ uncomment to save everything to a json file
-     with open(f"{path.replace('.vrt', '_threadData.json')}", 'w', encoding='utf8') as f:
-       json.dump(threadData, f, indent=2, ensure_ascii=False)
-     threadData.clear() """
+            with open(fjson, 'a+', encoding='utf8') as f:
+                json.dump(threadData, f, indent=2, ensure_ascii=False)
 
 
+ftxt, fjson = createAnalyzationFiles()
 s24_parser(path)
