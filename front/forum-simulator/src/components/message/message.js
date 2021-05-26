f5b9039cabc634c408666242e79ee6cc17c6d89e
Tomi Jokkeenhaara <tomi.jokkeenhaara@tuni.fi>
2021-05-26 08:50:09 +0300

    Added valence and arousal annotations to individual words, new test data

--- front/forum-simulator/src/components/message/message.js
+++ front/forum-simulator/src/components/message/message.js
@@ -12,11 +12,54 @@ const Message = ({ data, response }) => {
         } = data.commentMetadata;
 
         const words = data.words;
-        const message = _.map(words, wordData => {
-            if (_.includes([",", ".", "...", ":"], wordData.word)) {
-                return wordData.word;
+        const message = _.map(words, (wordData, index) => {
+
+
+            const valence = wordData.valence;
+            const arousal = Math.abs(wordData.arousal);
+
+            let valenceClass,
+                arousalClass;
+
+            if (valence < -0.75) {
+                valenceClass = -3;
+            } else if (valence < -0.5) {
+                valenceClass = -2;
+            } else if (valence < -0.25) {
+                valenceClass = -1;
+            } else if (valence < 0.25) {
+                valenceClass = 0;
+            } else if (valence < 0.5) {
+                valenceClass = 1;
+            } else if (valence < 0.75) {
+                valenceClass = 2;
+            } else if (valence <= 1) {
+                valenceClass = 3;
+            } else {
+                valenceClass = 0;
             }
-            return " " + wordData.word;
+
+            if (arousal < 0.25) {
+                arousalClass = 0;
+            } else if (arousal < 0.5) {
+                arousalClass = 1;
+            } else if (arousal < 0.75) {
+                arousalClass = 2;
+            } else if (arousal <= 1) {
+                arousalClass = 3;
+            } else {
+                arousalClass = 0;
+            }
+
+            const styledWord =
+                <span key={index} className={`arousal${arousalClass} valence${valenceClass}`}>
+                    {wordData.word}
+                </span>
+            const whitespace = <span> </span>
+            if (!_.includes([",", ".", "..", "...", ":", "!", "?", "\"", "'"], wordData.word)) {
+                return <span>{whitespace}{styledWord}</span>;
+            }
+            return styledWord;
         });
 
         const hasChildren = data.children && !_.isEmpty(data.children);
