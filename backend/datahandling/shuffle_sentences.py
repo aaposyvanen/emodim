933dd0a498eadd2d616467296d269ab8a151bdba
aaposyvanen <aapo.syvanen@gmail.com>
2021-06-10 11:05:04 +0300

    Textfile shuffling and independent exe for sentence classifying

---
+++ backend/datahandling/shuffle_sentences.py
@@ -0,0 +1,9 @@
+import random
+
+
+path = f"D:\\Work\\Data\\s24_2017_sentences_31-05-2021_15-17-17.txt"
+with open(path, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+random.shuffle(lines)
+with open(path.replace(".txt", "_shuffled.txt"), "w", encoding='utf-8') as f:
+    f.writelines(lines)
