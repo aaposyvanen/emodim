ee3891aa89295a37ffa60c28ccf53e3cf89c009b
Tomi Jokkeenhaara <tomi.jokkeenhaara@tuni.fi>
2021-06-21 16:37:15 +0300

    Fixed failing tests

--- front/message-analyzer/src/components/layout/layout.test.js
+++ front/message-analyzer/src/components/layout/layout.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "../../test-utils";
 import { Layout } from "./layout";
 
+jest.mock("../thread/thread", () => () => "Thread");
+
 test("Layout renders correctly", () => {
 
     const { container } = render(<Layout />);
