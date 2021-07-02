e598169a3411244995dfdd78d9cf26d3a8ed8093
Tomi Jokkeenhaara <tomi.jokkeenhaara@tuni.fi>
2021-07-02 13:44:26 +0300

    Changed backend/message-analyzer to function as chat server

--- backend/message-analyzer/analyze.py
+++
@@ -1,4 +0,0 @@
-import sys
-import emodim
-analysis = emodim.evaluate_text(sys.argv[1])
-print(analysis)
--- backend/message-analyzer/routes/index.js
+++ backend/message-analyzer/routes/index.js
@@ -1,21 +1,8 @@
 const express = require("express");
-const { spawn } = require("child_process")
 const router = express.Router();
 
-router.post("/", function(req, res, next) {
-
-  let dataToSend;
-
-  const python = spawn("python", ["analyze.py", req.body.text]);
-
-  python.stdout.on("data", function(data) {
-    dataToSend = data.toString();
-  });
-
-  python.on("close", (code) => {
-    console.log(`child process exiting with code ${code}`);
-    res.send(dataToSend)
-  });
+router.get("/", function(req, res, next) {
+  res.send({ response: "Chat server is alive" }).status(200);
 });
 
 module.exports = router;
