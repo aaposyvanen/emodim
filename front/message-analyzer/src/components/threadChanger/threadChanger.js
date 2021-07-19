40d1b5a9a9b20e0f29bae990c5e8d3921c8915ff
Tomi Jokkeenhaara <tomi.jokkeenhaara@tuni.fi>
2021-07-19 10:24:21 +0300

    Responsefield is now cleared when changing thread. Changed annotated version of message-analyzer to use port 3001.

--- front/message-analyzer/src/components/threadChanger/threadChanger.js
+++ front/message-analyzer/src/components/threadChanger/threadChanger.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import Button from '@material-ui/core/Button';
+import { updateMessageText } from "../../actions/responseActions";
 import "./threadChanger.css";
 
 const ThreadChanger = (props) => {
+    const dispatch = useDispatch();
 
     const increaseIndex = () => {
         if (props.currentIndex < props.maxIndex) {
             props.updateIndex(props.currentIndex + 1);
+            dispatch(updateMessageText(""));
+
         }
     };
 
     const decreaseIndex = () => {
         if (props.currentIndex > 0) {
             props.updateIndex(props.currentIndex - 1);
+            dispatch(updateMessageText(""));
+
         }
     };
 
